743368e9933e286e598eee65ed4e2a3f01432c94
colbyguan <colbyguan@gmail.com>
2017-06-12 00:20:37 -0700

    catch drop nonexistent collection error

--- scrape.js
+++ scrape.js
@@ -24,7 +24,13 @@ function done(db) {
   requestsCompleted += 1;
   if (requestsCompleted === urls.length) {
     const collection = db.collection('meta');
-    collection.drop().then((reply) => {
+    db.collections().then((collections) => {
+      if (collections.indexOf('meta') !== -1) {
+        return collections.drop();
+      } else {
+        return null;
+      }
+    }).then((reply) => {
       return collection.insertOne({
         lastModified: Date.now()
       });
@@ -42,58 +48,64 @@ function importArticles(err, db) {
   console.log("Connected successfully to mongodb");
   const collection = db.collection('articles');
   // Import from blank table
-  collection.drop();
+  db.collections().then((collections) => {
+    if (collections.indexOf('articles') !== -1) {
+      return collections.drop();
+    } else {
+      return null;
+    }
+  }).then((reply) => {
+    urls.forEach((url) => {
+      request(url, (error, response, html) => {
+        if (error) {
+          console.log(`Request error for ${url}`);
+          return;
+        }
+        const $ = cheerio.load(html);
 
-  urls.forEach((url) => {
-    request(url, (error, response, html) => {
-      if (error) {
-        console.log(`Request error for ${url}`);
-        return;
-      }
-      const $ = cheerio.load(html);
-
-      const category = url.substring(23, url.length - 1)
-      let articles = [];
-      // Page typically has 2 separate ul's, each of which contain list of articles
-      const $articleUls = $('ul.river');
-      $articleUls.each((i, elem) => {
-        $(elem).find('li.river-block').each((i, elem) => {
-          const document = {};
-          const $riverBlock = $(elem);
-          const href = $riverBlock.attr('data-permalink');
-          document.href = href;
-          // Each block is the div containing title, excerpt, etc.
-          // find() ~should~ return only 1 element;
-          const $blocks = $riverBlock.find('div.block-content', 'div.block-content-brief');
-          if ($blocks.length === 1) {
-            const $block = $blocks.first();
-            const title = $block.find('h2 a').text();
-            document.title = title;
-            // Excerpt is not always present
-            const $excerpt = $block.find('p')
-            if ($excerpt.length === 1) {
-              const excerptText = $excerpt.text().substring(0, 140);
-              document.excerpt = excerptText;
+        const category = url.substring(23, url.length - 1)
+        let articles = [];
+        // Page typically has 2 separate ul's, each of which contain list of articles
+        const $articleUls = $('ul.river');
+        $articleUls.each((i, elem) => {
+          $(elem).find('li.river-block').each((i, elem) => {
+            const document = {};
+            const $riverBlock = $(elem);
+            const href = $riverBlock.attr('data-permalink');
+            document.href = href;
+            // Each block is the div containing title, excerpt, etc.
+            // find() ~should~ return only 1 element;
+            const $blocks = $riverBlock.find('div.block-content', 'div.block-content-brief');
+            if ($blocks.length === 1) {
+              const $block = $blocks.first();
+              const title = $block.find('h2 a').text();
+              document.title = title;
+              // Excerpt is not always present
+              const $excerpt = $block.find('p')
+              if ($excerpt.length === 1) {
+                const excerptText = $excerpt.text().substring(0, 140);
+                document.excerpt = excerptText;
+              }
             }
-          }
-          // Tag is not always present
-          const $tags = $riverBlock.find('div.tags a span')
-          if ($tags.length === 1) {
-            const tag = $tags.text();
-            document.tag = tag;
-          }
-          if (document.title) {
-            document.category = category;
-            articles.push(document);
-          }
+            // Tag is not always present
+            const $tags = $riverBlock.find('div.tags a span')
+            if ($tags.length === 1) {
+              const tag = $tags.text();
+              document.tag = tag;
+            }
+            if (document.title) {
+              document.category = category;
+              articles.push(document);
+            }
+          });
         });
+        if (articles.length > 0) {
+          collection.insertMany(articles);
+        }
+        console.log(`Inserted ${articles.length} articles from ${url}`);
+        // Increment completion count
+        done(db);
       });
-      if (articles.length > 0) {
-        collection.insertMany(articles);
-      }
-      console.log(`Inserted ${articles.length} articles from ${url}`);
-      // Increment completion count
-      done(db);
     });
   });
 }
