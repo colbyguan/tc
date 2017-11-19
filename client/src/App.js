776540f99d05c0786aa44e65a9464c83eaee750a
colbyguan <colbyguan@gmail.com>
2017-11-18 17:30:12 -0800

    remove multi-column system. styled accordingly

--- client/src/App.js
+++ client/src/App.js
@@ -12,16 +12,7 @@ class App extends Component {
     this.state = {
       /* LRU queue to keep a maximum number of columns active at a time so
          text doesn't get too squished */
-      lru: ['popular'],
-      maxCategories: 4,
-      categories: {
-        'popular': true,
-        'startups': false,
-        'mobile': false,
-        'gadgets': false,
-        'social': false,
-        'enterprise': false
-      },
+      currCategory: 'popular',
       articles: [],
       lastModified: Date.now()
     }
@@ -32,63 +23,14 @@ class App extends Component {
       });
     });
   }
-  componentWillMount() {
-    this.updateMaxCategories();
-  }
-  componentDidMount() {
-    window.addEventListener('resize', this.updateMaxCategories.bind(this));
-  }
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.updateMaxCategories.bind(this));
-  }
-  updateMaxCategories() {
-    var w = window,
-        d = document,
-        documentElement = d.documentElement,
-        body = d.getElementsByTagName('body')[0],
-        width = w.innerWidth || documentElement.clientWidth || body.clientWidth;
-    let maxCategories = 4;
-    if (width < 769) {
-      maxCategories = 1;
-    } else if (width < 1200) {
-      maxCategories = 3;
-    }
-    let lru = this.state.lru;
-    let categories = this.state.categories
-    while (lru.length > maxCategories) {
-      categories[lru[0]] = false;
-      lru = lru.slice(1);
-    }
-    this.setState({
-      lru,
-      maxCategories
-    });
-  }
   toggleCategory(category) {
-    const categories = this.state.categories;
-    categories[category] = !categories[category];
-    let lru = this.state.lru;
-    if (categories[category]) {
-      // Pop off the least recently used if queue is full
-      if (lru.length === this.state.maxCategories) {
-        let popped = lru[0];
-        categories[popped] = false;
-        lru = lru.slice(1);
-      }
-      lru = [ ...lru, category];
-    } else {
-      // If we are turning a category off, also remove it from the queue
-      let idx = lru.indexOf(category);
-      lru = lru.slice(0, idx).concat(lru.slice(idx + 1));
-    }
     this.setState({
-      lru,
-      categories
+      currCategory: category
     });
   }
   renderCategory(category, i) {
     return (
-      <li key={i}><a className={!this.state.categories[category] ? 'inactive': ''} onClick={() => this.toggleCategory(category)}>
+      <li key={i}><a className={category != this.state.currCategory ? 'inactive': ''} onClick={() => this.toggleCategory(category)}>
         {category.charAt(0).toUpperCase() + category.slice(1)}
       </a></li>
     );
@@ -103,26 +45,12 @@ class App extends Component {
     );
   }
   renderListings() {
-    const columns = [];
-    let hitFirst = false;
-    allCategories.forEach((category, i) => {
-      const inactive = !this.state.categories[category];
       const articles = this.state.articles.filter((article) => {
-        return article.category === category;
+        return article.category === this.state.currCategory;
       });
-      columns.push(
-        <div key={category} className={"column " +
-          (this.state.maxCategories !== 1 ? 'category-column ' : '') +
-          (hitFirst ? 'left-border ' : '') +
-          (inactive ? 'is-inactive': '')}>
-          <Listing category={category} articles={articles} />
-        </div>
+      return (
+          <Listing category={this.state.currCategory} articles={articles} />
       );
-      if (!inactive) {
-        hitFirst = true;
-      }
-    });
-    return columns;
   }
   render() {
     return (
@@ -137,8 +65,10 @@ class App extends Component {
               </div>
             </div>
             <div className="column">
-              <div className="box">
-                {this.renderListings()}
+              <div className="listings">
+                <div className="box listings-box">
+                  {this.renderListings()}
+                </div>
               </div>
             </div>
           </div>
